8fcd7bc3529b94c83c8e2c23f70bd416632dc70e
anoob09 <aswarnkar.anup.swarnkar@gmail.com>
2020-05-15 23:19:38 +0530

    postgresql added

--- db_app.py
+++ db_app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://anoob09@localhost:3306/sputify'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 class Users1(db.Model):
     userid = db.Column(db.String(30), primary_key=True)
